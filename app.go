56945ff339e1498b0833ff8829813df1dce92bcc
yasasvi <yasasvigumma@gmail.com>
2024-09-16 20:09:52 +0530

    Moved it to a seperate file
    now we can run it with out any issues...

--- app.go
+++ app.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"Blood-Transmutation/config"
 	"Blood-Transmutation/server"
 	"Blood-Transmutation/webscraper"
 	"fmt"
-	"strings"
 )
 
 var (
@@ -13,14 +11,7 @@ var (
 )
 
 func main() {
-	for i := 1; i < 11; i++ {
-		data := webscraper.GetContent(config.Url + fmt.Sprint(strings.ReplaceAll(config.Questions[i], " ", "+")) + config.Pages + fmt.Sprint(i))
-		if data.Err != nil {
-			fmt.Println("Error:", data.Err)
-			return
-		}
-		data.ParceURL(&urlArr)
-	}
+  webscraper.RunScrape(&urlArr)
 	go server.StartServer(&urlArr)
 	fmt.Scanln()
 }
--- webscraper/scraper.go
+++ webscraper/scraper.go
@@ -1,7 +1,9 @@
 package webscraper
 
 import (
+	"Blood-Transmutation/config"
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"regexp"
@@ -14,18 +16,29 @@ type Scrape struct {
 	Err     error
 }
 
-func (s *Scrape) ParceURL(arr *[]string) {
+func RunScrape(urlArr *[]string) {
+	for i := 1; i < 11; i++ {
+		data := GetContent(config.Url + fmt.Sprint(strings.ReplaceAll(config.Questions[i], " ", "+")) + config.Pages + fmt.Sprint(i))
+		if data.Err != nil {
+			fmt.Println("Error:", data.Err)
+			return
+		}
+		data.ParceURL(&urlArr)
+	}
+}
+
+func (s *Scrape) ParceURL(arr **[]string) {
 	regex := regexp.MustCompile(`<a href="/url\?q=(.*?)&amp;`)
 	matches := regex.FindAllStringSubmatch(s.Content, -1)
 	for _, match := range matches {
 		if !strings.Contains(match[1], "google.com") && strings.Contains(match[1], "https") {
-		  *arr = append(*arr, match[1])
-    }
+			**arr = append(**arr, match[1])
+		}
 	}
 }
 
 func (s *Scrape) ParceTXT() {
-// need to implement this function with an nlp algorithm.
+	// need to implement this function with an nlp algorithm.
 }
 
 func GetContent(url string) Scrape {
@@ -37,13 +50,13 @@ func GetContent(url string) Scrape {
 			Err:     err,
 		}
 	}
-  if resp.StatusCode != http.StatusOK {
-    return Scrape{
-      Content: "",
-      Url:     url,
-      Err:     errors.New("Status code is not 200"),
-    }
-  }
+	if resp.StatusCode != http.StatusOK {
+		return Scrape{
+			Content: "",
+			Url:     url,
+			Err:     errors.New("Status code is not 200"),
+		}
+	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
