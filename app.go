ad0c6e9219d37dfb3dd0585e4c2e6ee5f0f6ba4f
yasasvi <yasasvigumma@gmail.com>
2024-10-28 18:53:26 +0530

    Rebuilding this application
    using open ai, should help a lot...

--- app.go
+++ app.go
@@ -1,17 +1,4 @@
 package main
 
-import (
-	"Blood-Transmutation/server"
-	"Blood-Transmutation/webscraper"
-	"fmt"
-)
-
-var (
-	urlArr = []string{}
-)
-
 func main() {
-  webscraper.RunScrape(&urlArr)
-	go server.StartServer(&urlArr)
-	fmt.Scanln()
 }
--- go.mod
+++ go.mod
@@ -1,7 +1,3 @@
 module Blood-Transmutation
 
 go 1.22.6
-
-require golang.org/x/net v0.29.0
-
-require github.com/joho/godotenv v1.5.1
